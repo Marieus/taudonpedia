f549055518ccc890fd8dee58b26d8ff34e62d879
Marius TAUDON <marius.taudon@groupe-ati.fr>
2026-04-14 14:57:10 +0200

    Creation de la page biologie.php

--- biologie.php
+++ biologie.php
@@ -2,9 +2,12 @@
 include 'inc/header.php'; 
 include 'dbconnect.php'; 
 
-$query = $pdo->query("SELECT especes.*, type.nom AS type_nom
+$query = $pdo->query("
+SELECT especes.*, type.nom AS type_nom, photo.chemin
 FROM especes
 JOIN type ON especes.id_type = type.id_type
+LEFT JOIN photo ON especes.id_especes = photo.id_especes
+GROUP BY especes.id_especes
 ");
 $especes = $query->fetchAll();
 ?>
@@ -13,6 +16,7 @@ $especes = $query->fetchAll();
     <h1>La Biologie</h1>
     <div class="menu"> <?php foreach($especes as $espece): ?>
             <div class="carte">
+                <img src="<?php echo !empty($espece['chemin']) ? $espece['chemin'] : 'images/default.jpg'; ?>" alt="Image de l'espèce">
                 <h3><?php echo htmlspecialchars($espece['nom']); ?></h3>
                 <p><em><?php echo htmlspecialchars($espece['nom_science']); ?></em></p>
                 <p>Type : <?php echo htmlspecialchars($espece['type_nom']); ?></p>
