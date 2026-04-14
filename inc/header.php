32ee30913e0519be30afe5cf2a3bca57ad74e238
Marius TAUDON <marius.taudon@groupe-ati.fr>
2026-04-14 12:50:53 +0200

    Initialisation du projet

---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title> Encyclopédie Sous Marine</title>
+</head>
+<body>
+
+<header>
+    <div class="banniere">
+        <a href="index.php" class="logo">
+            <div class="logo-image"><img src="images\LOGO_COP_300x300.png" alt="logo Caen Plongée"></div>
+            <div class="logo-texte">Taudon<span>Pédia</span></div>
+        </a>
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="biologie.php" <?php if(basename($_SERVER['PHP_SELF']) == 'biologie.php') echo 'class="active"'; ?>>Biologie</a>
+            <a href="epaves.php" <?php if(basename($_SERVER['PHP_SELF']) == 'epaves.php') echo 'class="active"'; ?>>Épaves</a>
+        </nav>    
+    </div>
+</header>
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include 'inc/header.php'; ?>
+<h1>Taudon<span>Pédia</span></h1>
+<h2>Bienvenue sur TaudonPedia, l'Encyclopédie des épaves et des espèces sous marine ! </h2>
+<p id="intro">
+    Ce site, a pour but de répertorier espèces et épaves aperçus durant diverses plongées, si vous avez un doute, vous pouvez vérifier ! 
+</p>
+<div class="menu">
+    <a href="biologie.php" class="carte">
+        <img src="images/biologie_bg.jpg" alt="">    
+        <h3>La Biologie</h3>
+        <p>Pour explorer la Faune et la Flore du monde sous-marin</p>
+        <span class="explorer">Explorer →</span>
+    </a>
+    
+    <a href="epaves.php" class="carte">  
+        <img src="images/barge_188.png" alt="">
+        <h3>Les Épaves</h3>
+        <p>Pour explorer les épaves sur lesquels les espèces ont été observées</p>
+        <span class="explorer">Explorer →</span>
+    </a>  
+</div>
+
+
+<?php include 'inc/footer.php'; ?>
