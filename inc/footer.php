fa8bcd48f9ee4ed1d01aacea6e17d62ec273c60f
Marius TAUDON <marius.taudon@groupe-ati.fr>
2026-04-16 14:27:15 +0200

    Ajoute du footer, début du responsive

--- inc/footer.php
+++ inc/footer.php
@@ -1,2 +1,7 @@
+<footer>
+    <p> TAUDONPÉDIA© | Tout droits réservés | en colaboration avec le club Caen Ouistrham Plongée</p>
+    <p> Pour toute demande concernant le site : <a href="mailto:marius@taudon.fr">marius@taudon.fr</a></p>
+</footer>
+
 </body>
 </html>
