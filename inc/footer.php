f0998d3760a76726c9d91e3a4a4d5dfa85364c19
Marius TAUDON <marius.taudon@groupe-ati.fr>
2026-04-17 12:58:44 +0200

    modification sur le footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,10 @@
 </main>
+
+
+
 <footer>
     <p> TAUDONPÉDIA © | Tout droits réservés | Marius TAUDON | <a href="mailto:marius@taudon.fr">marius@taudon.fr</a></p>
+    <p> Ce site est créé à la main, sans génération par Intelligence Artificielle </a></p>
 </footer>
 
 </body>
